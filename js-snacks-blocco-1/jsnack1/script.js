d09e80a17ae59cc8fa7afe38ef4cf7445f63b02f
lnannettik <l.nannetti.k@gmail.com>
2021-10-28 15:47:01 +0200

    push

---
+++ js-snacks-blocco-1/jsnack1/script.js
@@ -0,0 +1,92 @@
+// CHECH
+console.log(`JS OK`);
+
+// const primo = (prompt(`inserisci la prima parola`));
+// console.log(`${primo}`)
+
+// const secondo = (prompt(`inserisci la seconda parola`));
+// console.log(`${secondo}`)
+
+
+// // do (primo[i] < secondo[i])
+
+// // )
+
+// // if (primo.length < secondo.length) {
+// //     console.log(`${primo} + ${secondo}`)
+// // } else {
+// //     console.log(`${secondo} ${primo}`)
+// // }
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+                 
+// let numero = 0
+
+// for (numero = 0; numero < 6; numero++) {
+//     let dato = parseInt( prompt( 'Dammi un numero' ) );
+//     console.log(`${dato}`)
+
+//     let somma = [dato    ]
+
+
+
+// }
+
+// SNACK 4
+
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array
+
+let list = [];
+console.log(list.length)
+
+
+for (i = 0; i < 6; i++) {
+    let dato = parseInt( prompt( 'Dammi un numero' ) );
+    console.log(`${dato}`)
+
+    if (dato % 2 !== 0) {
+        list.push(`${dato}`)
+    }
+   
+    console.log(list)
+
+
+}
+                                     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js-snacks-blocco-2/jsnack1/script.js
@@ -0,0 +1,51 @@
+// CHECK
+console.log('OK JS')
+
+
+// JSNACK2
+
+// Inserisci un numero, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+
+
+// let num = parseInt( prompt('Dammi un numero'));
+
+// if (num % 2 == 0) {
+//     console.log(num)
+// } else {
+//     let numdisp = `(${num} + 1) ` 
+// }
+
+
+
+// JSNACK3
+
+// Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+// lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+
+let nomi = ['Pdor', 'Kmer', 'Ajeje']
+console.log(nomi)
+
+let cognomi = ['Muriel', 'Belotti', 'Raspadori']
+console.log(cognomi)
+
+let lista = [...nomi, ...cognomi]
+console.log(lista)
+
+
+
+// JSNACK4
+
+// Crea un array di numeri interi
+// e fai la somma di tutti gli elementi che sono in posizione dispari
+
+
+
+
+
+// JSNACK5
+
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi casuali all’array che ha meno elementi,
+// fino a quando ne avrà tanti quanti l’altro.
